d812b821e814ee0cb43be25a69884b2a409de03d
omkar3720 <omkarshinde2728@gmail.com>
2024-10-16 21:23:00 +0530

    add in central Repositary

---
+++ demoProjectForGit/src/demoProjectForGit/main/Employee.java
@@ -0,0 +1,19 @@
+package demoProjectForGit.main;
+
+public class Employee 
+{
+   public int m1()
+   {
+	   return 10;
+   }
+   public String m2()
+   {
+	   return "Java";
+   }
+   public static void main(String[] args) 
+   {
+	Employee e = new Employee();
+	e.m1();
+	e.m2();
+}
+}
